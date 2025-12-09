241271393e9f93008b06ce2b621308b4cd970c8c
taylin damonze <82763280+taylin1@users.noreply.github.com>
2025-12-09 12:31:40 +0200

    Added UI for login and signup page

--- frontend-notes/src/App.jsx
+++ frontend-notes/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import LoginPage from './Pages/LoginPage'
+import SignUpPage from './Pages/SignUpPage'
 function App() {
   
 
   return (
     <>
+    <SignUpPage/>
       
     </>
   )
---
+++ frontend-notes/src/Pages/LoginPage.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <div className="bg-slate-900 h-screen flex justify-center items-center">
+        <div className="flex flex-col items-center">
+
+        <h1 className="text-white mb-4 font-bold text-xl">
+            Noti Application
+        </h1>
+
+        <form className="bg-gray-800 w-100 rounded-lg p-6 ">
+          <h1 className="text-2xl text-white font-bold text-center mb-4">Login:</h1>
+          
+          <div className=" space-x-8 mb-4">
+          <label className="text-white ">Email:</label>
+          <input
+            type="email"
+            placeholder="Please enter email"
+            className="input bg-gray-200 rounded-xl py-2 "
+            value=""
+          />
+          </div>
+
+          <div className=" space-x-5">
+          <label className="text-white">Password:</label>
+          <input
+            type="password"
+            placeholder="Please enter password"
+            className="input bg-gray-200 rounded-xl py-2"
+            value=""
+          />
+          </div>
+          <p className="mt-8 text-white">Dont have an account sign up now: </p>
+          <div className="">
+            <button className="bg-indigo-800 rounded-xl text-white p-2 space-x-4">Sign up</button>
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
