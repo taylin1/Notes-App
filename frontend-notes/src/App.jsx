598cd5d73663f5a5671ac5fc2ba19659fcbd61b5
taylin damonze <82763280+taylin1@users.noreply.github.com>
2025-12-13 09:02:43 +0200

    Added Browser Route

--- frontend-notes/src/App.jsx
+++ frontend-notes/src/App.jsx
@@ -2,12 +2,19 @@ import './App.css'
 import LoginPage from './Pages/LoginPage'
 import SignUpPage from './Pages/SignUpPage'
 import Dashboard from './Pages/Dashboard'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
   
 
   return (
     <>
-    <LoginPage/>
+   <BrowserRouter>
+   <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+   </Routes>
+   </BrowserRouter>
       
     </>
   )
