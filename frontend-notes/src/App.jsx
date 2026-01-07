ac61a016575bd3485548ad5cb603d80ee269ada6
taylin damonze <82763280+taylin1@users.noreply.github.com>
2026-01-07 14:36:16 +0200

    Updated build script

--- frontend-notes/src/App.jsx
+++ frontend-notes/src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import Dashboard from './pages/Dashboard'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 function App() {
   
 
